f5939b1c0c4d9ae60b435e2414c9df51e35e9f55
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-06 00:20:01 -0700

    Updated

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+let friends = [
+    {
+        'name': 'Fenando',
+        'photo':'https://vignette.wikia.nocookie.net/swordartonline/images/a/ab/Kirito_sprite.png/revision/latest?cb=20140613022319',
+        'scores':[
+            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+        ]
+    }
+]
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,3 @@
+const exp = require('express');
+const path = require('path');
+const app = exp();
