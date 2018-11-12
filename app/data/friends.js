9844c9dee348446fbcff9f56bbb2dbbc6bdab296
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-11 23:00:24 -0700

    work in progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 let friends = [
     {
         'name': 'Fenando',
-        'photo':'https://vignette.wikia.nocookie.net/swordartonline/images/a/ab/Kirito_sprite.png/revision/latest?cb=20140613022319',
-        'scores':[
-            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+        'pic':'https://vignette.wikia.nocookie.net/swordartonline/images/a/ab/Kirito_sprite.png/revision/latest?cb=20140613022319',
+        'answers':[
+           " 5", "1", "4", "4", "5", "1", "2", "5", "4", "1"
         ]
     }
 ]
